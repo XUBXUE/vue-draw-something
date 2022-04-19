92684093f5e99a3f29abf8931cae7567dbc02ffa
xuboxue <2370237499@qq.com>
2022-04-19 17:29:37 +0800

    push code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@ export const debounce = (fn: Function, delay: number = 500) => {
     timer && clearTimeout(timer);
     const arg = [...arguments];
     timer = setTimeout(() => {
-      fn.apply(this, arg);
+      fn.apply(null, arg);
     }, delay);
   };
 };
